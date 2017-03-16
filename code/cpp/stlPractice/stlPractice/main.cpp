092bd1699e40f8f839db077fc7dbef392146be97
kapon2016 <kapon2016@LeoMBP.local>
2017-03-16 23:14:15 +0800

    set proj

---
+++ code/cpp/stlPractice/stlPractice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  stlPractice
+//
+//  Created by kapon2016 on 2017/3/16.
+//  Copyright © 2017年 kapon2016. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    cout << "Hello, World!\n";
+    return 0;
+}
